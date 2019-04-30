a1f75757a04947f7797b56560540de42f9e5410b
raoli1 <50125853+raoli1@users.noreply.github.com>
2019-04-30 10:36:47 -0400

    Add files via upload

--- finalproject.py
+++ finalproject.py
@@ -1,5 +1,6 @@
 import csv
-import pandas as pd 
+import pandas as pd
+#from sklearn import preprocessing 
 #replace missing values with an unique category
 data=pd.read_csv("qudditch_training.csv")
 columns_replace=["house","player_code","move_specialty"]
@@ -16,6 +17,20 @@ df.drop(["id_num","player_id","weight","quidditch_league_player"], axis=1,inplac
 df=pd.get_dummies(df, columns=["gender","house","foul_type_id","game_move_id","penalty_id","player_code","move_specialty","player_type"])
 df.insert(len(df.columns),"quidditch_league_player", df_target) 
 #do label encoding
+#version 1
+ordered_satisfaction = ["None", "Norm", ">7", ">8", ">200", ">300"]
+cat_dtype = pd.api.types.CategoricalDtype(ordered_satisfaction, ordered=True)
+df["snitchnip"]=df["snitchnip"].astype(cat_dtype).cat.codes
+df["stooging"]=df["stooging"].astype(cat_dtype).cat.codes
+#version 2
+'''
+ordered_satisfaction = ["None", "Norm",  ">200", ">300"]
+cat_dtype = pd.api.types.CategoricalDtype(ordered_satisfaction, ordered=True)
+df["snitchnip"]=df["snitchnip"].astype(cat_dtype).cat.codes
+ordered_satisfaction = ["None", "Norm",  ">7", ">8"]
+cat_dtype = pd.api.types.CategoricalDtype(ordered_satisfaction, ordered=True)
+df["stooging"]=df["stooging"].astype(cat_dtype).cat.codes
+'''
 #print(df)
 df.to_csv("a.csv",index=False)
 #df_target.to_csv("b.csv",index=False)
